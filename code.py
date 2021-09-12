9199ac5455e2a0004f1361ab003e602630628012
sayantan-kuila <52849803+sayantan-kuila@users.noreply.github.com>
2021-09-12 21:00:36 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import plotly.express as px
+url = 'http://api.open-notify.org/iss-now.json'
+df = pd.read_json(url)
+df['latitude'] = df.loc['latitude','iss_position']
+df['longitude'] = df.loc['longitude','iss_position']
+df.reset_index(inplace=True)
+df = df.drop(['index','message'],axis=1)
+figure = px.scatter_geo(df,lat='latitude',lon='longitude')
+figure.show()
